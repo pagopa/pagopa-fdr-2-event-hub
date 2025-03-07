ecf7accb5d1ab8340c0f7e9bb8c900074dbecdbd
Angelo Caporaso <56113767+cap-ang@users.noreply.github.com>
2025-03-07 17:12:34 +0100

    [PAGOPA-2686] fix(util): Update parse date method (#14)
    * [PAGOPA-2686] fix(util): Update parse date method
    
    * [PAGOPA-2686] fix(util): Add test case
    
    * Bump to version 1.0.3-1-PAGOPA-2686-fix-parse-date [skip ci]
    
    * [PAGOPA-2686] log(host.json): Set to information
    
    ---------
    
    Co-authored-by: pagopa-github-bot <github-bot@pagopa.it>

---
+++ src/test/java/it/gov/pagopa/fdr/to/eventhub/util/FlussoRendicontazioneMapperTest.java
@@ -0,0 +1,38 @@
+package it.gov.pagopa.fdr.to.eventhub.util;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.LocalDateTime;
+
+import static it.gov.pagopa.fdr.to.eventhub.mapper.FlussoRendicontazioneMapper.parseDate;
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+public class FlussoRendicontazioneMapperTest {
+
+    @Test
+    void testParseDate() {
+        String[] dates = new String[] {
+            "2025-03-06T11:01:36+01:00",
+            "2025-03-06T11:01:36",
+            "2025-03-06T05:01:36-05:00",
+            "2025-03-06T10:01:36Z",
+        };
+
+        for (String date : dates) {
+            LocalDateTime localDateTime = parseDate(date);
+            assertNotNull(localDateTime);
+            assertEquals("2025-03-06T11:01:36", localDateTime.toString());
+        }
+    }
+
+    @Test
+    void testParseDateWithoutHH() {
+        String date = "2025-03-06";
+        LocalDateTime localDateTime = parseDate(date);
+        assertNotNull(localDateTime);
+        assertEquals("2025-03-06T00:00", localDateTime.toString());
+    }
+}
