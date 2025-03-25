815360248fa1e496c8d40025adb1f41c716ff96c
Andrea De Rinaldis <andrea.derinaldis@pagopa.it>
2025-03-25 11:20:13 +0100

    [PAGOPA-2718] feat: adding new error code for FDR3 section

--- src/main/java/it/gov/pagopa/fdr/to/eventhub/util/ErrorCodes.java
+++ src/main/java/it/gov/pagopa/fdr/to/eventhub/util/ErrorCodes.java
@@ -8,7 +8,9 @@ public enum ErrorCodes {
   COMMON_E1("FDR-E1", "Error while sending to EventHub."),
   COMMON_E2("FDR-E2", "Error while process XML Blob."),
   // FDR1 Errors
-  FDR1_E1("FDR1-E1", "Error processing Blob in processFDR1BlobFiles function");
+  FDR1_E1("FDR1-E1", "Error processing Blob in processFDR1BlobFiles function"),
+  // FDR3 Errors
+  FDR3_E1("FDR3-E1", "Error processing Blob in processFDR3BlobFiles function");
 
   private final String code;
   private final String message;
