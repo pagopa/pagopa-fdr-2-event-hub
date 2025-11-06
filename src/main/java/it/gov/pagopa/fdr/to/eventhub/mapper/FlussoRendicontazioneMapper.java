fb39b3b458288c635ac9a554335dd6d4870b6fc8
Francesco Cesareo <cesareo.francesco@gmail.com>
2025-11-06 09:22:07 +0100

    Apply suggestion from @github-actions[bot]
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/main/java/it/gov/pagopa/fdr/to/eventhub/mapper/FlussoRendicontazioneMapper.java
+++ src/main/java/it/gov/pagopa/fdr/to/eventhub/mapper/FlussoRendicontazioneMapper.java
@@ -18,7 +18,6 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
-
 import lombok.Getter;
 import lombok.Setter;
 import lombok.experimental.UtilityClass;
